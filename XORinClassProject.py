eecae3e5c9aceca964bb94fd86a558a66ae93726
bread sticks <uubixx.0208@gmail.com>
2024-02-28 21:27:54 +0000

    in class xor

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -1,3 +1,5 @@
+import math 
+
 characters = [
     # lowercase characters
 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
@@ -17,3 +19,62 @@ def decode(binary):
     charIndex = int(binary, 2)
     return characters[charIndex]
 
+def xor(bit1, bit2):
+    if bit1 == bit2:
+        return '0'
+    else :
+        return '1'
+
+
+def XORonByte (byte, key):
+
+    emsg = ""
+
+    for i in range(len(byte)):
+        emsg += xor(byte[i], key[i])
+
+    return emsg
+
+def XORonLetter(letter, keyLetter):
+    letterBin = encode(letter)
+    keyLetterBin = encode(keyLetter)
+
+    encryptedLetter = XORonByte(letterBin, keyLetterBin)
+
+    return decode(encryptedLetter)
+
+def XORonSentence(sentence, key):
+
+    encryptedSentence = ""
+    genKey = generateKey(sentence, key)
+
+    for i in range(len(sentence)):
+        encryptedSentence += XORonLetter(sentence[i], genKey[i])
+
+    return encryptedSentence
+
+
+
+def generateKey(message, key):
+    if len(message) == len(key):
+        return key
+
+    elif len(message) < len(key):
+        return key[0: len(message)]
+
+    else:
+        genKey = ""
+        rep = math.floor(len(message)/len(key))
+        remain = len(message) % len(key)
+
+        for i in range(rep):
+            genKey += key
+
+        genKey += key[0:remain]
+
+        return genKey
+
+msg = input("enter a message ")
+key = input("enter a key ")
+
+print("your encrypted message is:", XORonSentence(msg, key))
